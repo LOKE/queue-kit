c07a9ce22f3f2b95a1d7a0b47a0afe925997fe32
Den Williams <dennis.williams@loke.com.au>
2024-09-03 18:18:19 +1000

    metric names feedback

--- src/metrics.ts
+++ src/metrics.ts
@@ -1,23 +1,24 @@
 import { Counter, Histogram, Registry } from "prom-client";
 
 export const messagesReceivedCounter = new Counter({
-  name: "queue_messages_received_total",
+  name: "queue_handler_messages_total",
   help: "Total number of messages received from the queue",
   labelNames: ["queue", "provider"],
 });
 export const messagesFailedCounter = new Counter({
-  name: "queue_messages_failed_total",
+  name: "queue_handler_failures_total",
   help: "Total number of messages failed to handle",
   labelNames: ["queue", "provider"],
 });
+export const messageHandlerDuration = new Histogram({
+  name: "queue_handler_duration_seconds",
+  help: "Queue message handler processing time in seconds.",
+  labelNames: ["queue", "provider"],
+});
+
 export const messagesQueuedCounter = new Counter({
   name: "queue_messages_queued_total",
   help: "Total number of messages published to the queue",
-  labelNames: ["queue", "provider"],
-});
-export const messageHandlerDuration = new Histogram({
-  name: "queue_message_handler_duration_ms",
-  help: "Queue message handler processing time in milliseconds.",
   labelNames: ["queue", "provider"],
 });
 
